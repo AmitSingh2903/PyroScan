fa121b66ab945409b90db703f2eeb88e28c852d4
Amit Singh <96614509+AmitSingh2903@users.noreply.github.com>
2025-04-13 04:09:53 +0530

    Update cloudModel.ts

--- src/lib/cloudModel.ts
+++ src/lib/cloudModel.ts
@@ -2,8 +2,12 @@ import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-backend-webgl';
 
 // Initialize WebGL backend
-await tf.setBackend('webgl');
-await tf.ready();
+async function initTF() {
+  await tf.setBackend("webgl");
+  await tf.ready();
+}
+initTF();
+
 
 // Constants for image processing
 const IMAGE_SIZE = 224;
